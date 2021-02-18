9d57a97e318edfad4ffc75cbbde6d82144a7a212
Lebeg134 <cactusmoovies@gmail.com>
2021-02-18 22:31:38 +0100

    Initial commit

---
+++ src/main/java/hu/lebeg134/TFC_PH_compat/TFC_PH_compat.java
@@ -0,0 +1,30 @@
+package hu.lebeg134.TFC_PH_compat;
+
+import hu.lebeg134.TFC_PH_compat.proxy.CommonProxy;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+public class TFC_PH_compat
+{
+    @Mod.Instance(value = Reference.MODID)
+    public static TFC_PH_compat instance = new TFC_PH_compat();
+
+    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
+    public static CommonProxy proxy;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+}
---
+++ src/main/java/hu/lebeg134/TFC_PH_compat/proxy/CommonProxy.java
@@ -0,0 +1,8 @@
+package hu.lebeg134.TFC_PH_compat.proxy;
+
+import net.minecraft.item.Item;
+
+public class CommonProxy
+{
+    public void registerModel(Item item, int metadata) {}
+}

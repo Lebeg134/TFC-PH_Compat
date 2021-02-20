6a41f2bcf6c2a33bd65e26e014991d07a6e597e2
Lebeg134 <cactusmoovies@gmail.com>
2021-02-20 16:45:24 +0100

    PH ItemSeedFoods are now non plantable

--- src/main/java/hu/lebeg134/tpc/util/handlers/PHPlantEventHandler.java
+++ src/main/java/hu/lebeg134/tpc/util/handlers/PHPlantEventHandler.java
@@ -1,6 +1,12 @@
 package hu.lebeg134.tpc.util.handlers;
 
 import com.pam.harvestcraft.blocks.CropRegistry;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
@@ -11,11 +17,9 @@ public class PHPlantEventHandler {
     @SubscribeEvent(priority = EventPriority.LOW)
     public static void PlayerInterract (PlayerInteractEvent.RightClickBlock event)
     {
-        System.out.println("Rightclick on block detected");
         if (CropRegistry.getFoods().values().contains(event.getItemStack().getItem()))
         {
-            System.out.println("rightclick PH SeedFoodItem on block detected");
+            event.setCanceled(true);
         }
-
     }
 }

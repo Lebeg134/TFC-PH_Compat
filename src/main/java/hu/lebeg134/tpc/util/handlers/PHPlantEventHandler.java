17a087954f8f78c30a77c30b603409869db620ff
Lebeg134 <cactusmoovies@gmail.com>
2021-02-20 17:07:03 +0100

    Added dead crop textures

--- src/main/java/hu/lebeg134/tpc/util/handlers/PHPlantEventHandler.java
+++ src/main/java/hu/lebeg134/tpc/util/handlers/PHPlantEventHandler.java
@@ -1,12 +1,6 @@
 package hu.lebeg134.tpc.util.handlers;
 
 import com.pam.harvestcraft.blocks.CropRegistry;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;

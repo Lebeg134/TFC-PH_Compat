6f42c7ff0a25aba46d29ea7e62526aa632f7c873
Lebeg134 <cactusmoovies@gmail.com>
2021-02-21 11:16:55 +0100

    Removed unused imports

--- src/main/java/hu/lebeg134/tpc/objects/items/ItemsTPC.java
+++ src/main/java/hu/lebeg134/tpc/objects/items/ItemsTPC.java
@@ -1,29 +1,22 @@
 package hu.lebeg134.tpc.objects.items;
 
 import com.google.common.collect.ImmutableList;
-import com.pam.harvestcraft.blocks.CropRegistry;
-import com.pam.harvestcraft.item.ItemRegistry;
 import hu.lebeg134.tpc.util.agriculture.TPCrop;
 import hu.lebeg134.tpc.util.agriculture.TPFood;
 import net.dries007.tfc.api.capability.food.CapabilityFood;
-import net.dries007.tfc.api.capability.food.FoodData;
 import net.dries007.tfc.api.capability.food.FoodHandler;
 import net.dries007.tfc.objects.inventory.ingredient.IIngredient;
 import net.dries007.tfc.objects.items.ItemSeedsTFC;
 import net.dries007.tfc.util.OreDictionaryHelper;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.Ingredient;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.registries.IForgeRegistry;
 
-import static com.pam.harvestcraft.blocks.CropRegistry.SPICELEAF;
 import static hu.lebeg134.tpc.Reference.MODID;
-import static net.dries007.tfc.TerraFirmaCraft.MOD_ID;
 import static net.dries007.tfc.objects.CreativeTabsTFC.CT_FOOD;
 
 @Mod.EventBusSubscriber(modid = MODID)

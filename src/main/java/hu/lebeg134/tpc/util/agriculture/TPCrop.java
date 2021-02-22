abf5143807f2895036adf4f5ca4c6c51ec5b3255
Lebeg134 <cactusmoovies@gmail.com>
2021-02-22 17:53:51 +0100

    Finished up adding crops

--- src/main/java/hu/lebeg134/tpc/util/agriculture/TPCrop.java
+++ src/main/java/hu/lebeg134/tpc/util/agriculture/TPCrop.java
@@ -1,6 +1,7 @@
 package hu.lebeg134.tpc.util.agriculture;
 
 import com.pam.harvestcraft.blocks.CropRegistry;
+import com.pam.harvestcraft.blocks.FruitRegistry;
 import net.dries007.tfc.api.types.ICrop;
 import net.dries007.tfc.objects.blocks.agriculture.BlockCropDead;
 import net.dries007.tfc.objects.blocks.agriculture.BlockCropSimple;
@@ -31,8 +32,28 @@ import java.util.function.Supplier;
 
 
 public enum TPCrop implements ICrop {
-    SPICELEAF(() -> new ItemStack(CropRegistry.getFood(CropRegistry.SPICELEAF)),() -> ItemStack.EMPTY,-5f,0f,30f,40f,50f,100f,400f,450f,6,0.5f, CropType.SIMPLE),
-    BROCCOLI(() -> new ItemStack(CropRegistry.getFood(CropRegistry.BROCCOLI)), () -> ItemStack.EMPTY, 2, 9, 35, 41, 70, 150, 410, 450, 6, 0.6f, CropType.SIMPLE);
+    BROCCOLI(() -> new ItemStack(CropRegistry.getFood(CropRegistry.BROCCOLI)), () -> ItemStack.EMPTY, 2, 9, 35, 41, 70, 150, 410, 450, 6, 0.6f, CropType.SIMPLE),
+    CELERY(() -> new ItemStack(CropRegistry.getFood(CropRegistry.CELERY)), () -> ItemStack.EMPTY, 2, 9, 35, 41, 70, 150, 410, 450, 6, 0.5f, CropType.SIMPLE),
+    CHILI_PEPPER(() -> new ItemStack(CropRegistry.getFood(CropRegistry.CHILIPEPPER)), () -> ItemStack.EMPTY, 5, 10, 40, 45, 50, 70, 400, 450, 6, 0.55f, CropType.PICKABLE),
+    COFFEEBEAN(() -> new ItemStack(CropRegistry.getFood(CropRegistry.COFFEE)), () -> ItemStack.EMPTY, 5, 10, 40, 45, 50, 100, 400, 450, 6, 0.5f, CropType.SIMPLE),
+    CUCUMBER(() -> new ItemStack(CropRegistry.getFood(CropRegistry.CUCUMBER)), () -> ItemStack.EMPTY, 3, 10, 30, 36, 50, 100, 400, 450, 6, 0.6f, CropType.SIMPLE),
+    EGGPLANT(() -> new ItemStack(CropRegistry.getFood(CropRegistry.EGGPLANT)), () -> ItemStack.EMPTY, 5, 14, 33, 37, 45, 90, 390, 440, 6, 0.6f, CropType.SIMPLE),
+    GINGER(() -> new ItemStack(CropRegistry.getFood(CropRegistry.GINGER)), () -> ItemStack.EMPTY, -5, 0, 30, 40, 50, 100, 400, 450, 6, 0.5f, CropType.SIMPLE),
+    GRAPE(() -> new ItemStack(CropRegistry.getFood(CropRegistry.GRAPE)), () -> ItemStack.EMPTY, 0, 8, 36, 40, 50, 120, 390, 430, 6, 0.5f, CropType.PICKABLE),
+    LETTUCE(() -> new ItemStack(CropRegistry.getFood(CropRegistry.LETTUCE)), () -> ItemStack.EMPTY, -10, 0, 27, 33, 50, 60, 280, 300, 6, 0.6f, CropType.SIMPLE),
+    MUSHROOM(() -> new ItemStack(CropRegistry.getFood(CropRegistry.WHITEMUSHROOM)), () -> ItemStack.EMPTY, -10, 0, 40, 45, 50, 60, 450, 500, 6, 0.45f, CropType.SIMPLE),
+    MUSTARDSEEDS(() -> new ItemStack(CropRegistry.getFood(CropRegistry.MUSTARD)), () -> ItemStack.EMPTY, 5, 10, 35, 40, 50, 100, 400, 450, 6, 0.5f, CropType.SIMPLE),
+    PEANUT(() -> new ItemStack(CropRegistry.getFood(CropRegistry.PEANUT)), () -> ItemStack.EMPTY, 0, 4, 30, 35, 50, 100, 390, 440, 6, 0.55f, CropType.SIMPLE),
+    PEAS(() -> new ItemStack(CropRegistry.getFood(CropRegistry.PEAS)), () -> ItemStack.EMPTY, 2, 9, 35, 41, 70, 150, 410, 450, 6, 0.5f, CropType.PICKABLE),
+    PEPPERCORN(() -> new ItemStack(FruitRegistry.getFood(FruitRegistry.PEPPERCORN)), () -> ItemStack.EMPTY, 5, 10, 40, 45, 50, 100, 400, 450, 6, 0.5f, CropType.PICKABLE),
+    RADISH(() -> new ItemStack(CropRegistry.getFood(CropRegistry.RADISH)), () -> ItemStack.EMPTY, 2, 9, 35, 41, 70, 150, 410, 450, 6, 0.6f, CropType.SIMPLE),
+    SCALLION(() -> new ItemStack(CropRegistry.getFood(CropRegistry.SCALLION)), () -> ItemStack.EMPTY, 2, 9, 35, 41, 70, 150, 410, 450, 6, 0.5f, CropType.SIMPLE),
+    SESAMESEEDS(() -> new ItemStack(CropRegistry.getFood(CropRegistry.SESAME)), () -> ItemStack.EMPTY, 2, 9, 35, 41, 70, 150, 410, 450, 6, 0.5f, CropType.SIMPLE),
+    SPICELEAF(() -> new ItemStack(CropRegistry.getFood(CropRegistry.SPICELEAF)), () -> ItemStack.EMPTY, -5, 0, 30, 40, 50, 100, 400, 450, 6, 0.5f, CropType.SIMPLE),
+    SPINACH(() -> new ItemStack(CropRegistry.getFood(CropRegistry.SPINACH)), () -> ItemStack.EMPTY, -10, 0, 27, 33, 50, 60, 280, 300, 6, 0.6f, CropType.SIMPLE),
+    SWEETPOTATO(() -> new ItemStack(CropRegistry.getFood(CropRegistry.SWEETPOTATO)), () -> ItemStack.EMPTY, 0, 4, 30, 35, 50, 100, 390, 440, 6, 0.55f, CropType.SIMPLE),
+    TEALEAF(() -> new ItemStack(CropRegistry.getFood(CropRegistry.TEALEAF)), () -> ItemStack.EMPTY, -5, 0, 30, 40, 50, 100, 400, 450, 6, 0.6f, CropType.SIMPLE),
+    ZUCCHINI(() -> new ItemStack(CropRegistry.getFood(CropRegistry.ZUCCHINI)), () -> ItemStack.EMPTY, 3, 10, 30, 36, 50, 100, 400, 450, 6, 0.6f, CropType.SIMPLE);
 
 
 

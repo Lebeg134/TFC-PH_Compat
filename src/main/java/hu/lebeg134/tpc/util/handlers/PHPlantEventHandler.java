6730f74a5914b26f591821a00fbad80ea20454da
Lebeg134 <cactusmoovies@gmail.com>
2021-03-04 15:00:10 +0100

    Fixed PH crops not being able to put in storage drawers

--- src/main/java/hu/lebeg134/tpc/util/handlers/PHPlantEventHandler.java
+++ src/main/java/hu/lebeg134/tpc/util/handlers/PHPlantEventHandler.java
@@ -1,6 +1,7 @@
 package hu.lebeg134.tpc.util.handlers;
 
 import com.pam.harvestcraft.blocks.CropRegistry;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
@@ -11,7 +12,8 @@ public class PHPlantEventHandler {
     @SubscribeEvent(priority = EventPriority.LOW)
     public static void PlayerInterract (PlayerInteractEvent.RightClickBlock event)
     {
-        if (CropRegistry.getFoods().values().contains(event.getItemStack().getItem()))
+        if (CropRegistry.getFoods().values().contains(event.getItemStack().getItem()) &&
+            event.getWorld().getBlockState(event.getPos()).getBlock().getRegistryName().getPath().contains("farmland"))
         {
             event.setCanceled(true);
         }

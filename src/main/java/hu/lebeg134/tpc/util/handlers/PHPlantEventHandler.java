fbcb713ced9e4b17a3963a91a2349288d0fa6c17
Lebeg134 <cactusmoovies@gmail.com>
2021-03-04 15:25:58 +0100

    Fixed PH crops not being able to put in storage drawers

--- src/main/java/hu/lebeg134/tpc/util/handlers/PHPlantEventHandler.java
+++ src/main/java/hu/lebeg134/tpc/util/handlers/PHPlantEventHandler.java
@@ -1,7 +1,6 @@
 package hu.lebeg134.tpc.util.handlers;
 
 import com.pam.harvestcraft.blocks.CropRegistry;
-import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;

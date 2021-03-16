052865b233bd8debead419f8045ad0e254c6b5ec
Lebeg134 <cactusmoovies@gmail.com>
2021-03-16 17:56:15 +0100

    Added Raisins recipe to CA compat

--- src/main/java/hu/lebeg134/tfc_ph_compat/compat/CaffeineAddonCompat.java
+++ src/main/java/hu/lebeg134/tfc_ph_compat/compat/CaffeineAddonCompat.java
@@ -1,10 +1,12 @@
 package hu.lebeg134.tfc_ph_compat.compat;
 
+import com.eerussianguy.firmalife.recipe.DryingRecipe;
 import com.pam.harvestcraft.blocks.CropRegistry;
 import com.pam.harvestcraft.item.ItemRegistry;
 import hu.lebeg134.tfc_ph_compat.types.RecipeModifier;
 import hu.lebeg134.tfc_ph_compat.util.handlers.OreDictHandler;
 import net.dries007.tfc.objects.inventory.ingredient.IIngredient;
+import net.dries007.tfc.util.calendar.ICalendar;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.util.ResourceLocation;
@@ -33,13 +35,16 @@ public class CaffeineAddonCompat {
         RecipeModifier.dummyOutRecipe(recipeRegistry, "harvestcraft:coffeeitem");
         //RecipeModifier.dummyOutRecipe(recipeRegistry, "harvestcraft:teaitem");
         RecipeModifier.dummyOutRecipe(recipeRegistry, "harvestcraft:driedsoupitem");
+        RecipeModifier.dummyOutRecipe(recipeRegistry, "harvestcraft:raisinsitem");
     }
     @SubscribeEvent
     public static void onRegisterDryingMatRecipeEvent(RegistryEvent.Register<DryingMatRecipe> event)
     {
+        int hour = ICalendar.TICKS_IN_HOUR;
         IForgeRegistry<DryingMatRecipe> r = event.getRegistry();
         r.registerAll(
-                new DryingMatRecipe(IIngredient.of(ItemRegistry.stockItem), new ItemStack(ItemRegistry.driedsoupItem),24000).setRegistryName("dried_soup_drying1")
+                new DryingMatRecipe(IIngredient.of(ItemRegistry.stockItem), new ItemStack(ItemRegistry.driedsoupItem),24000).setRegistryName("dried_soup_drying1"),
+                new DryingMatRecipe(IIngredient.of("cropGrape"),new ItemStack(ItemRegistry.raisinsItem), 24*hour).setRegistryName("raisins_drying2")
         );
     }
 

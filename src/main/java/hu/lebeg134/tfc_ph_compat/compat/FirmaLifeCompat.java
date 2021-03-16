8cab4215fe638eb85f789d76da916f89aafb1a6f
Lebeg134 <cactusmoovies@gmail.com>
2021-03-16 17:56:58 +0100

    Balanced new accesible foods from v0.2.2

--- src/main/java/hu/lebeg134/tfc_ph_compat/compat/FirmaLifeCompat.java
+++ src/main/java/hu/lebeg134/tfc_ph_compat/compat/FirmaLifeCompat.java
@@ -1,36 +1,45 @@
 package hu.lebeg134.tfc_ph_compat.compat;
 
+import com.eerussianguy.firmalife.init.FoodFL;
 import com.eerussianguy.firmalife.recipe.DryingRecipe;
+import com.eerussianguy.firmalife.registry.ItemsFL;
 import com.pam.harvestcraft.item.ItemRegistry;
 import hu.lebeg134.tfc_ph_compat.types.RecipeModifier;
 import hu.lebeg134.tfc_ph_compat.util.handlers.OreDictHandler;
 import net.dries007.tfc.objects.inventory.ingredient.IIngredient;
+import net.dries007.tfc.util.calendar.ICalendar;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.registries.IForgeRegistry;
 import net.minecraftforge.registries.IForgeRegistryModifiable;
 
 public class FirmaLifeCompat {
     public static void InitFirmaLife(){
         //OreDict addition
-        /*
-        OreDictionary.registerOre("cropPumpkin", new ItemStack(ItemsFL.PUMPKIN_CHUNKS));
-        OreDictionary.registerOre("cropPumpkin", new ItemStack(ItemsFL.PUMPKIN_SCOOPED));
-        OreDictionary.registerOre("listAllveggie", new ItemStack(ItemsFL.PUMPKIN_CHUNKS));
-        OreDictionary.registerOre("listAllveggie", new ItemStack(ItemsFL.PUMPKIN_SCOOPED));
-        OreDictionary.registerOre("foodChocolatebar", new ItemStack(ItemsFL.DARK_CHOCOLATE));
-        OreDictionary.registerOre("foodChocolatebar", new ItemStack(ItemsFL.MILK_CHOCOLATE));
+
+        OreDictionary.registerOre("cropPumpkin", new ItemStack(ItemsFL.getFood(FoodFL.PUMPKIN_CHUNKS)));
+        OreDictionary.registerOre("cropPumpkin", new ItemStack(ItemsFL.getFood(FoodFL.PUMPKIN_SCOOPED)));
+        OreDictionary.registerOre("listAllveggie", new ItemStack(ItemsFL.getFood(FoodFL.PUMPKIN_CHUNKS)));
+        OreDictionary.registerOre("listAllveggie", new ItemStack(ItemsFL.getFood(FoodFL.PUMPKIN_SCOOPED)));
+        OreDictionary.registerOre("foodChocolatebar", new ItemStack(ItemsFL.getFood(FoodFL.DARK_CHOCOLATE)));
+        OreDictionary.registerOre("foodChocolatebar", new ItemStack(ItemsFL.getFood(FoodFL.MILK_CHOCOLATE)));
         OreDictionary.registerOre("foodCocoapowder", new ItemStack(ItemsFL.COCOA_POWDER));
         OreDictionary.registerOre("cropCinnamon", new ItemStack(ItemsFL.CINNAMON));
         OreDictionary.registerOre("foodGroundcinnamon", new ItemStack(ItemsFL.GROUND_CINNAMON));
-        OreDictionary.registerOre("cropCoconut", new ItemStack(ItemsFL.COCONUT));
-        OreDictionary.registerOre("foodFlour", new ItemStack(ItemsFL.CHESTNUT_FLOUR));
-        OreDictionary.registerOre("foodDough", new ItemStack(ItemsFL.CHESTNUT_DOUGH));
-        OreDictionary.registerOre("foodBread", new ItemStack(ItemsFL.CHESTNUT_BREAD));
-        OreDictionary.registerOre("bread", new ItemStack(ItemsFL.CHESTNUT_BREAD));
-         */
+        OreDictionary.registerOre("cropCoconut", new ItemStack(ItemsFL.getFood(FoodFL.COCONUT)));
+        OreDictionary.registerOre("foodFlour", new ItemStack(ItemsFL.getFood(FoodFL.CHESTNUT_FLOUR)));
+        OreDictionary.registerOre("foodDough", new ItemStack(ItemsFL.getFood(FoodFL.CHESTNUT_DOUGH)));
+        OreDictionary.registerOre("foodBread", new ItemStack(ItemsFL.getFood(FoodFL.CHESTNUT_BREAD)));
+        OreDictionary.registerOre("bread", new ItemStack(ItemsFL.getFood(FoodFL.CHESTNUT_BREAD)));
+        OreDictionary.registerOre("cropChestnut", new ItemStack(ItemsFL.getFood(FoodFL.CHESTNUTS)));
+        OreDictionary.registerOre("foodRoastedchestnut", new ItemStack(ItemsFL.getFood(FoodFL.ROASTED_CHESTNUTS)));
+        OreDictionary.registerOre("cropPecan", new ItemStack(ItemsFL.getFood(FoodFL.PECANS)));
+        OreDictionary.registerOre("cropPineapple", new ItemStack(ItemsFL.getFood(FoodFL.PINEAPPLE_CHUNKS)));
+
+
         //OreDict remove
         OreDictHandler.removeAll(ItemRegistry.chocolatebarItem);
         OreDictHandler.removeAll(ItemRegistry.cocoapowderItem);
@@ -44,13 +53,18 @@ public class FirmaLifeCompat {
         RecipeModifier.dummyOutRecipe(recipeRegistry, "harvestcraft:coconutmilkitem");
         RecipeModifier.dummyOutRecipe(recipeRegistry, "harvestcraft:toastedcoconutitem");
         RecipeModifier.dummyOutRecipe(recipeRegistry, "harvestcraft:groundcinnamonitem");
+        RecipeModifier.dummyOutRecipe(recipeRegistry, "harvestcraft:pinacoladaitem");
+        RecipeModifier.dummyOutRecipe(recipeRegistry, "harvestcraft:chocolatebaritem_x4");
+        RecipeModifier.dummyOutRecipe(recipeRegistry, "harvestcraft:raisinsitem");
     }
     @SubscribeEvent
     public static void onRegisterDryingRecipeEvent(RegistryEvent.Register<DryingRecipe> event)
     {
+        int hour = ICalendar.TICKS_IN_HOUR;
         IForgeRegistry<DryingRecipe> r = event.getRegistry();
         r.registerAll(
-                new DryingRecipe(IIngredient.of(ItemRegistry.stockItem), new ItemStack(ItemRegistry.driedsoupItem),24000).setRegistryName("dried_soup_drying2")
+                new DryingRecipe(IIngredient.of(ItemRegistry.stockItem), new ItemStack(ItemRegistry.driedsoupItem),24*hour).setRegistryName("dried_soup_drying2"),
+                new DryingRecipe(IIngredient.of("cropGrape"),new ItemStack(ItemRegistry.raisinsItem), 24*hour).setRegistryName("raisins_drying2")
         );
     }
 }
